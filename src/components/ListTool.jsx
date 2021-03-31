12a4c7af286ce735023a2ba13fa4143e521f45eb
Lucas Santos <synluke777@gmail.com>
2021-03-31 07:03:41 -0300

    Função Delete alterada

--- src/components/ListTool.jsx
+++ src/components/ListTool.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap';
-import DeleteTool from './DeleteTool'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import services from '../services'
 
 const ListTool = ({ id,title, link, description, tags }) => {
+
+    const deleteTool = async () => {
+        try {
+            await services.deleteTool(id);
+            alert('Tool deleted!')
+            window.location.reload(false);
+        }catch(error){
+            console.log(error)
+            alert('Delete Photo failed!')
+        }
+    }
 
     return (
         <>
@@ -11,14 +24,14 @@ const ListTool = ({ id,title, link, description, tags }) => {
                     <Col><h3><a href={link} rel="noopener noreferrer" target="_blank" class="titleItemTool">{title}</a></h3></Col>
                     <Col md="auto">
                         
-                       <DeleteTool/>     
+                    <button onClick={deleteTool} type="button" class="btn buttonDelete" ><FontAwesomeIcon icon={faTimes} /> remove</button>  
 
                     </Col>
                 </Row>
                 <Row>
                     <Col>
                         <p>{description}</p>
-                        {!tags ? <span class="font-monospace">no tag</span> : tags.map((tag, j) => <span class="font-monospace" id={j}> #{tag} </span>)}
+                        {!tags ? <span class="font-monospace">no tag</span> : tags.map((tag, j) => <span class="font-monospace" key={j}> #{tag} </span>)}
                     </Col>
                 </Row>
             </Col>
